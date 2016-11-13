8c8a1e53ab0263fc6afc935b109b88bd13468951
shmulzi <shmulprivate@Shmuliks-MacBook-Pro.local>
2016-11-13 15:20:51 +0200

    continue maman12

--- 20441/maman12/src/Date.java
+++ 20441/maman12/src/Date.java
@@ -1,13 +1,36 @@
+/**
+ * This class represents a date - a day, month and year.
+ */
 public class Date {
 
-    private final int DEFAULT_YEAR = 2000;
-    private final int DEFAULT_MONTH = 1;
-    private final int DEFAULT_DAY = 1;
+    //constant values
+    private static final int LEAP_YEAR_DIVIDER = 4;
+    private static final int JANUARY = 1;
+    private static final int FEBRUARY = 2;
+    private static final int MARCH = 3;
+    private static final int APRIL = 4;
+    private static final int JUNE = 6;
+    private static final int SEPTEMBER = 9;
+    private static final int NOVEMBER = 11;
+    private static final int DECEMBER = 12;
+    private static final int DEFAULT_YEAR = 2000;
+    private static final int DEFAULT_MONTH = 1;
+    private static final int DEFAULT_DAY = 1;
+    private static final int MIN_NUM_OF_DAYS = 1;
+    private static final int MIN_YEAR = 1000;
+    private static final int MAX_YEAR = 9999;
 
+    //private variables
     private int _day;
     private int _month;
     private int _year;
 
+    /**
+     * takes in a day, month and year. If the date input is not valid, the default values are set.
+     * @param day the day of the date. default value is 1
+     * @param month the moth of the date. default value is 1
+     * @param year the year of the date. default value is 2000
+     */
     public Date(int day, int month, int year)
     {
         if(isValidDate(day,month,year)){
@@ -22,6 +45,10 @@ public class Date {
         }
     }
 
+    /**
+     *  copies an existing instance of Date.
+     * @param date the date that should be copied
+     */
     public Date(Date date)
     {
         _day = date.getDay();
@@ -29,21 +56,37 @@ public class Date {
         _year = date.getYear();
     }
 
+    /**
+     * returns the day of the date.
+     * @return the day of the date.
+     */
     public int getDay()
     {
         return _day;
     }
 
+    /**
+     * returns the month of the date.
+     * @return the month of the date.
+     */
     public int getMonth()
     {
         return _month;
     }
 
+    /**
+     * returns the year of the date.
+     * @return the year of the date.
+     */
     public int getYear()
     {
         return _year;
     }
 
+    /**
+     * sets the day of the date. if day is not valid, former value is kept.
+     * @param day the day of the date. between 1 and the number of days in that month.
+     */
     public void setDay(int day)
     {
         if(isValidDate(day,_month,_year)){
@@ -51,6 +94,10 @@ public class Date {
         }
     }
 
+    /**
+     * sets the month of the date. if month is not valid, former value is kept.
+     * @param month the month of the date. between 1 and 12.
+     */
     public void setMonth(int month)
     {
         if(isValidDate(_day,month,_year)){
@@ -58,6 +105,10 @@ public class Date {
         }
     }
 
+    /**
+     * sets the year of the date. if year is not valid, former value is kept.
+     * @param year the year of the date. between 1000 and 9999.
+     */
     public void setYear(int year)
     {
         if(isValidDate(_day,_month,year)){
@@ -65,11 +116,21 @@ public class Date {
         }
     }
 
+    /**
+     * returns true if this instance of Date to the give instance.
+     * @param date the instance of Date to compare to.
+     * @return true if this instance of Date to the give instance.
+     */
     public boolean equals(Date date)
     {
         return date.getYear() == _year && date.getMonth() == _month && date.getDay() == date._day;
     }
 
+    /**
+     * returns true the given date is before this instance.
+     * @param date
+     * @return
+     */
     public boolean before(Date date)
     {
         return date.getYear() < _year || date.getMonth() < _month || date.getDay() < _day;
@@ -95,17 +156,17 @@ public class Date {
 
     private boolean isValidDate(int day, int month, int year)
     {
-        return year > 0 && month > 1 && month < 12 && day > 1 && day < calcDaysInMonth(month,year);
+        return year >= MIN_YEAR && year <= MAX_YEAR && month >= JANUARY && month <= DECEMBER && day >= MIN_NUM_OF_DAYS && day <= calcDaysInMonth(month,year);
     }
 
     private int calcDaysInMonth(int month, int year)
     {
         int daysInMonth = 31;
-        if(month == 4 || month == 6 || month == 9 || month == 11){
+        if(month == APRIL || month == JUNE || month == SEPTEMBER || month == NOVEMBER){
             daysInMonth = 30;
         }
-        if(month == 2){
-            if(year % 4 == 0){
+        if(month == FEBRUARY){
+            if(year % LEAP_YEAR_DIVIDER == 0){
                 daysInMonth = 29;
             }
             else {
@@ -117,7 +178,7 @@ public class Date {
 
     private int calculateDate(int day, int month, int year)
     {
-        if(month < 3){
+        if(month < MARCH){
             year--;
             month = month+12;
         }
