0c6bb94a6e0ba2211c3b9b3c2baa0f4f046a513e
shmulzi <samya2005@gmail.com>
2016-11-22 18:04:54 +0200

    maman11 comments and updates

--- 20441/maman1/src/Dates.java
+++ 20441/maman1/src/Dates.java
@@ -12,14 +12,9 @@ public class Dates {
     private static final int LEAP_YEAR_DIVIDER = 4;
     private static final int JANUARY = 1;
     private static final int FEBRUARY = 2;
-    private static final int MARCH = 3;
     private static final int APRIL = 4;
-    private static final int MAY = 5;
     private static final int JUNE = 6;
-    private static final int JULY = 7;
-    private static final int AUGUST = 8;
     private static final int SEPTEMBER = 9;
-    private static final int OCTOBER = 10;
     private static final int NOVEMBER = 11;
     private static final int DECEMBER = 12;
 
--- 20441/maman1/src/Milliseconds.java
+++ 20441/maman1/src/Milliseconds.java
@@ -1,20 +1,24 @@
 import java.util.Scanner;
 
+/**
+ * This class will run a program that reads an integer that represents milliseconds from the user, and represent it in a days:hours:minutes:seconds
+ */
 public class Milliseconds {
 
+    //constant values
     private static final long SECONDS_TO_MS = 1000;
     private static final long MINUTES_TO_MS = SECONDS_TO_MS * 60;
     private static final long HOURS_TO_MS = MINUTES_TO_MS * 60;
     private static final long DAYS_TO_MS = HOURS_TO_MS * 24;
 
-
+    //begin program
     public static void main(String[] args){
-
+        //we ask the user for an integer that represents milliseconds, then take it in with an instance of java Scanner class
         Scanner scan = new Scanner(System.in);
         System.out.println("This program reads an integer which represents Milliseconds and converts it to days, hours, minutes and seconds. ");
         System.out.println("Please enter the number of Milliseconds");
         long ms = scan.nextLong();
-
+        //we calculate the seconds, minutes, hours and days using modulo remainder.
         long days = ms / DAYS_TO_MS;
         long remainder = ms % DAYS_TO_MS;
         long hours = remainder / HOURS_TO_MS;
@@ -22,8 +26,7 @@ public class Milliseconds {
         long minutes = remainder / MINUTES_TO_MS;
         remainder = ms % MINUTES_TO_MS;
         long seconds = remainder / SECONDS_TO_MS;
-
+        //we output the data we have calculated to the user
         System.out.println(days + " days " + hours + ":" + minutes + ":" + seconds + " hours");
-    }
-
+    } // end of program
 }
