6786efe179ad50f92de466b76721f0e808d8f0fc
shmulzi <shmulprivate@Shmuliks-MacBook-Pro.local>
2017-01-28 20:51:34 +0200

    maman14

--- 20441/maman14/src/Ex14.java
+++ 20441/maman14/src/Ex14.java
@@ -30,153 +30,152 @@ public class Ex14 {
      * @return the largest number of integers in the array that can be summed up in to a number that can be divided by 3
      * without a remainder.
      */
-    public static int what(int[] a)
+    public static int what(int a[])
     {
-        int temp = 0;
-        int outer = 0, inner = 0;
-        for (int i = 0; i < a.length; i++){
-            outer++;
-            for(int j = i; j < a.length; j++){
-                inner++;
-                int c = f(a,i,j);
-                if(c % 3 == 0){
-                    if(j-i+1 > temp) {
-                        temp = j - i + 1;
-                    }
-                }
-            }
-        }
-        System.out.println("outer = " + outer);
-        System.out.println("inner = " + inner);
-        return temp;
-    }
-
-
-
-
-    public static int what3(int a[])
-    {
-        int totalSum = f(a,0,a.length-1);
-        for(int i = 0; i < a.length; i++){
-            int tempSum = f(a,i,a.length-1);
-            for(int j = a.length-1; j > i; j--){
-                if(tempSum % 3 == 0){
-                    System.out.println("what3 j = " + j + ", i = " + i + ", tempSum = " + tempSum);
-                    return j-i+1;
-                }
-                tempSum -= a[j];
-            }
-        }
-        return 0;
-    }
-
-    public static int what4(int a[])
-    {
-        int totalSum = f(a,0,a.length-1);
-        if(totalSum % 3 == 0){
-            return a.length;
-        }
-
-        for(int i = 1; i < a.length; i++){
-            totalSum -= a[i];
-            int tempSum = totalSum;
-            int low = 0, high = a.length-1;
-            boolean step = false;
-            while(low <= high){
-                if(step){
-                    tempSum -= a[low++];
-                }
-                else {
-                    tempSum -= a[high--];
-                }
-                if(tempSum % 3 == 0){
-                    return high-low+1;
-                }
-                step = !step;
-            }
-        }
-        return 0;
-    }
-
-
-    public static int what2(int a[])
-    {
-        int outer = 0, inner = 0;
         for(int numOfSlots = a.length; numOfSlots > 0; numOfSlots--){ //worst case O(n)
-            outer++;
             for(int i = 0; i <= a.length-numOfSlots; i++){ //n+(n-1)+(n-2)...
-                inner++;
                 int b = f(a,i,i+numOfSlots-1);
                 if(b % 3 == 0){
-                    System.out.println("outer = " + outer);
-                    System.out.println("inner = " + inner);
                     return numOfSlots;
                 }
             }
         }
-        System.out.println("outer = " + outer);
-        System.out.println("inner = " + inner);
         return 0;
+    }
+
+    public static int betterWhat(int a[])
+    {
+        int f0 = -1, f1 = -1, f2 = -1, l0, l1, l2, d0 = -1, d1 = -1, d2 = -1, total = 0;
+
+        for(int i = 0; i < a.length; i++){
+            total += abs(a[i]);
+            int totalMod = total % 3;
+            switch (totalMod) {
+                case 0:
+                    if(f0 == -1){
+                        f0 = i;
+                    }
+                    break;
+                case 1:
+                    if(f1 == -1){
+                        f1 = i;
+                    }
+                    break;
+                case 2:
+                    if(f2 == -1){
+                        f2 = i;
+                    }
+                    break;
+                default:
+                    System.out.println("something went wrong, mod is not in range.");
+                    break;
+            }
+        }
+
+        total = 0;
+
+        for(int i = a.length-1; i >= 0; i--){
+            total += abs(a[i]);
+            int totalMod = total % 3;
+            switch (totalMod) {
+                case 0:
+                    if(d0 == -1){
+                        d0 = abs(i - f0);
+                    }
+                    break;
+                case 1:
+                    if(d1 == -1){
+                        d1 = abs(i - f1);
+                    }
+                    break;
+                case 2:
+                    if(d2 == -1){
+                        d2 = abs(i - f2);
+                    }
+                    break;
+                default:
+                    System.out.println("something went wrong, mod is not in range.");
+            }
+        }
+        return max(d0,d1,d2);
+    }
+
+    private static int abs(int a)
+    {
+        return a < 0 ? -a : a;
+    }
+
+    private static int max(int a, int b, int c)
+    {
+        if(a > b && a > c){
+            return a;
+        }
+        else if (b > a && b > c){
+            return b;
+        }
+        else if (c > a && c > b) {
+            return c;
+        }
+        return a;
     }
 
     /**
      * Goes through the given array of integers (expecting zeros and ones, at least one zero) and marks the distance in
      * array cells of 1's to the nearest 0's
+     * time complexity - O(N)
      * @param a an integer array that includes at least one zero, and is made only of 0s and 1s
      */
     public static void zeroDistance(int a[])
-    {
+    {/**
+     * Goes through the given array of integers (expecting zeros and ones, at least one zero) and marks the distance in
+     * array cells of 1's to the nearest 0's
+     * @param a an integer array that includes at least one zero, and is made only of 0s and 1s
+     */
+        int counter = 1;
+        int mid = -1;
+        int firstZeroIndex = -1;
+        boolean reachedLastZero = false;
 
-        int begin = 0;
-        int count = 0;
-
-        for(int i = 0; i < a.length; i++){ // O(n)
-            if(a[i] == 0){
-                if(begin == 0 && count > 1){
-                    int addition = 1;
-                    for(int j = begin+1; j < i; j++){
-                        a[j] += addition++;
-                    }
-                }
-                else if(count > 2){
-                    boolean isEven = (count % 2 == 0);
-                    int mid = begin + (isEven ? (count/2) : (count/2)+1);
-                    int addition = 1;
-                    for(int j = begin+2; j < i-1; j++){
-                        a[j] += addition;
-                        if(j == mid){
-                            if(isEven){
-                                mid++;
-                                isEven = false;
-                            }
-                            else {
-                                addition--;
-                            }
-                        }
-                        else if(j < mid) {
-                            addition++;
-
-                        }
-                        else {
-                            addition--;
-                        }
-                    }
-                }
-                count = 0;
-                begin = i;
+        //we run through the array once from begin to end, counting up from 1 up until the next 0 slot, where we reset
+        //the counter - we also mark the first zero index for a later calculation. (There must be one since we are guaranteed
+        //one in the requirements)
+        for(int i = 0; i < a.length; i++){
+            if(a[i] != 0){
+                a[i] = counter++;
             }
             else {
-                count++;
+                if(firstZeroIndex == -1){
+                    firstZeroIndex = i;
+                }
+                counter = 1;
             }
         }
 
-        if(count > 1){
-            int addition = 1;
-            for(int i = begin+2; i < a.length; i++){
-                a[i] += addition++;
+        //we run through the array again, this time end to beginning. Counting again, but now only in certain conditions -
+        //1. we have not reached the mid value of the counted cells (we know this because we know the highest value from
+        //counting forward
+        //2. we have not reached the first zero index we marked previously
+        //3. we have reached the last zero in the array
+        for(int i = a.length-1; i >= 0; i--){
+            if(a[i] != 0){
+                if (mid == -1){
+                    mid = a[i] / 2;
+                    if(reachedLastZero){
+                        counter = 1;
+                    }
+                }
+                if((a[i] > mid || i <= firstZeroIndex) && reachedLastZero){
+                    a[i] = counter++;
+                }
+            }
+            else {
+                if(!reachedLastZero)
+                    reachedLastZero = true;
+                mid = -1;
             }
         }
     }
+
 
     /**
      * Returns true if the second given string is a transformation of the first given string
@@ -188,9 +187,14 @@ public class Ex14 {
      */
     public static boolean isTrans(String s, String t)
     {
-        if(s.length() == 0 || t.length() == 0){
-            return false;
+        //if strings are null or empty, then t is a transformation of s
+        if(s == null && t == null){
+            return true;
         }
+        if(s.length() == 0 || t.length() == 0){
+            return true;
+        }
+        //if the length of s is 1, then we can only run through t and confirmed all characters there match the on in s
         if(s.length() == 1){
             if(t.length() == 1) {
                 if (t.charAt(0) == s.charAt(0)) {
@@ -202,11 +206,12 @@ public class Ex14 {
             }
             return isTrans(s,t.substring(1));
         }
-
-        // [a,a,b,c]
-        // [a,b,c]
+        //if the current character at location 0 match, we need to further investigate
         if(s.charAt(0) == t.charAt(0)){
+            //if a character appears in location 0 and in location 1 of t, there might be a transformation
             if(t.charAt(0) == t.charAt(1)){
+                //we check if there is a double character also in s, if so, we can move forward in both strings, if not,
+                //only move forward in t
                 if(s.charAt(0) == s.charAt(1)){
                     return isTrans(s.substring(1),t.substring(1));
                 }
@@ -215,6 +220,8 @@ public class Ex14 {
                 }
             }
             else {
+                //if the next letter in t is different than the current one, and in s they are the same, there is no
+                //transformation, return false
                 if(s.charAt(0) == s.charAt(1)){
                     return false;
                 }
@@ -307,50 +314,52 @@ public class Ex14 {
 
     public static void main(String[] args)
     {
-        int[] a = new int[10];
-        for(int i = 0; i < a.length; i++){
-            a[i] = new Random().nextInt(50)-25;
-            System.out.print(a[i] + ", ");
-        }
-        System.out.println();
-        System.out.println("original output - " + what(a));
-        System.out.println("what2 output - " + what2(a));
-        System.out.println("what4 output - " + what4(a));
-
-
-
-        int b[] = new int[] {1,1,1,0,1,1,1,1,1,0,1,1,1};
-        for(int i = 0; i < b.length; i++){
-            System.out.print("["+b[i]+"]");
-        }
-        System.out.println();
-        zeroDistance(b);
-        for(int i = 0; i < b.length; i++){
-            System.out.print("["+b[i]+"]");
-        }
-        System.out.println();
-        String str1 = "abbc";
-        String str2 = "aabbcccccccccccccd";
-        System.out.println("str1 = " + str1 + ", str2 = " + str2 + ", isTrans? " + isTrans(str1,str2));
-        System.out.println();
-
-        int[] d = new int[5];
-        int[] pat = new int[5];
-
-        for(int i = 0; i < d.length; i++){
-            d[i] = new Random().nextInt(21);
-            pat[i] = new Random().nextInt(3);
+        for(int f = 0; f < 10; f++){
+            int[] a = new int[10];
+            for(int i = 0; i < a.length; i++){
+                a[i] = new Random().nextInt(50)-25;
+                System.out.print("["+a[i]+"]");
+            }
+            System.out.println();
+            System.out.println("what  - " + what(a));
+            System.out.println("betterWhat  - " + betterWhat(a));
         }
 
-        for(int i = 0; i < d.length; i++){
-            System.out.print("["+d[i]+"]");
-        }
-        System.out.println();
-        for(int i = 0; i < pat.length; i++){
-            System.out.print("["+pat[i]+"]");
-        }
-        System.out.println();
-        System.out.println("match = " + match(d,pat));
+
+//
+//        int b[] = new int[] {1,1,1,0,1,1,1,1,1,0,1,1,1};
+//        int c[] = new int[] {1,1,1,0,1,1,1,1,1,0,1,1,1};
+//        for(int i = 0; i < b.length; i++){
+//            System.out.print("["+b[i]+"]");
+//        }
+//        System.out.println();
+//        zeroDistance(b);
+//        for(int i = 0; i < b.length; i++){
+//            System.out.print("["+b[i]+"]");
+//        }
+//        System.out.println();
+//        String str1 = "abbc";
+//        String str2 = "aabbcccccccccccccd";
+//        System.out.println("str1 = " + str1 + ", str2 = " + str2 + ", isTrans? " + isTrans(str1,str2));
+//        System.out.println();
+//
+//        int[] d = new int[5];
+//        int[] pat = new int[5];
+//
+//        for(int i = 0; i < d.length; i++){
+//            d[i] = new Random().nextInt(21);
+//            pat[i] = new Random().nextInt(3);
+//        }
+//
+//        for(int i = 0; i < d.length; i++){
+//            System.out.print("["+d[i]+"]");
+//        }
+//        System.out.println();
+//        for(int i = 0; i < pat.length; i++){
+//            System.out.print("["+pat[i]+"]");
+//        }
+//        System.out.println();
+//        System.out.println("match = " + match(d,pat));
     }
 
 }
