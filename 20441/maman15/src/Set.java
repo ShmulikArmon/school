312222d4e6374143db20c577856c9406ab4a2695
Shmulzi <samya2005@gmail.com>
2017-02-01 22:53:46 +0200

    no message

--- 20441/maman15/src/Set.java
+++ 20441/maman15/src/Set.java
@@ -1,16 +1,19 @@
-/**
- * Created by Shmuliks on 29-Jan-17.
- *
- *
- */
+
 public class Set {
 
     private IntNode _head;
-    private IntNode _tail;
+    private IntNode _tail; //?
+
+    private int _numOfElements; //??
 
     public Set()
     {
 
+    }
+
+    private boolean isUnevenAndPositive(int num)
+    {
+        return num > 0 && num % 2 != 0;
     }
 
     public boolean isEmpty()
@@ -37,7 +40,7 @@ public class Set {
     public boolean equals(Set other){
         IntNode curr = _head;
         IntNode otherCurr = other._head;
-        while(curr.getNext() != null){
+        while(curr != null){
             if(otherCurr == null || curr.getValue() != otherCurr.getValue()){
                 return false;
             }
@@ -49,6 +52,107 @@ public class Set {
         return true;
     }
 
+    public boolean isMemeber(int num)
+    {
+        if(isUnevenAndPositive(num)){
+            IntNode curr = _head;
+            while (curr != null){
+                if(curr.getValue() > num){
+                    return false;
+                }
+                else if(curr.getValue() == num){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
+    public boolean subSet(Set other)
+    {
+        IntNode curr = _head;
+        IntNode otherCurr = other._head;
+        while (curr != null){
+            if(otherCurr.getValue() == curr.getValue()){
+                otherCurr = otherCurr.getNext();
+            }
+            if(otherCurr == null){
+                return true;
+            }
+            curr = curr.getNext();
+        }
+        return false;
+    }
+
+    public void addToSet(int x)
+    {
+        if(isUnevenAndPositive(x)){
+            if(_head == null){
+                _head = new IntNode(x,null);
+            }
+            else {
+                IntNode curr = _head;
+                while(curr.getNext() != null){
+                    if(x == curr.getValue()){
+                        return;
+                    }
+                    if(x > curr.getValue()){
+                        IntNode nextNode = curr.getNext();
+                        curr.setNext(new IntNode(x,nextNode));
+                        return;
+                    }
+                    curr = curr.getNext();
+                }
+                curr.setNext(new IntNode(x,null));
+            }
+        }
+    }
+
+    public void removeFromSet(int x)
+    {
+        if(isUnevenAndPositive(x)){
+            IntNode curr = _head;
+            if(_head.getValue() == x){
+                _head = curr.getNext();
+                return;
+            }
+            while(curr.getNext() != null){
+                if(curr.getValue() > x){
+                    return;
+                }
+                if(curr.getNext().getValue() == x){
+                    curr.setNext(curr.getNext().getNext());
+                    return;
+                }
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        if(_head == null){
+            return "{}";
+        }
+        return "{"+toString(_head);
+    }
+
+    private String toString(IntNode node)
+    {
+        if(node.getNext() == null){
+            return node.getValue() + "}";
+        }
+        return node.getValue() + "," + toString(node.getNext());
+
+    }
+
+    public static void main(String[] args)
+    {
+        Set A = new Set();
+        A.addToSet(1);
+        A.addToSet(3);
+        A.addToSet(7);
+        A.addToSet(5);
+        System.out.println(A);
+    }
 
 }
